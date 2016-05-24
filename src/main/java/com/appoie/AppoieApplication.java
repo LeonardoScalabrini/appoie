9c4b193d20757fc5288fbfbc7bda63554dd1593d
Igor Duarte <Igor Duarte>
2016-05-24 20:46:02 -0300

    Adicionando Projeto

---
+++ src/main/java/com/appoie/AppoieApplication.java
@@ -0,0 +1,12 @@
+package com.appoie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppoieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppoieApplication.class, args);
+	}
+}
