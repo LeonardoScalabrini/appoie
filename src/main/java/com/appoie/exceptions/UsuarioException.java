cfe69c99752a441def5f01edf647f62b43d1fb5d
leonardo scalabrini <leonardo scalabrini>
2016-06-05 19:04:22 -0300

    Refatorado o projeto, adicionado testes e rooldback das configurações do banco e portas

--- src/main/java/com/appoie/exceptions/UsuarioException.java
+++
@@ -1,8 +0,0 @@
-package com.appoie.exceptions;
-
-public class UsuarioException extends Exception {
-	private static final long serialVersionUID = 1L;
-		public UsuarioException(String mensagem) {
-			super(mensagem);
-		}
-}
